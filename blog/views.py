3ede416342f2cd1a43d38f0697ea542a92ca1ffc
이한영 <dev@lhy.kr>
2019-12-11 14:41:45 +0900

    post_list view에서 복잡한 HTML문자열을 리턴

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,20 @@ from django.http import HttpResponse
 
 
 def post_list(request):
-    return HttpResponse('<html><body><h1>Post List!</h1></body></html>')
+    return HttpResponse(
+        '<html>'
+        '<body>'
+        '<h1>Post List!</h1>'
+        '<div>'
+        '<p>published: 12.11.2019, 14:38</p>'
+        '<h2><a href="">My First post</a></h2>'
+        '<p>asdfasdfasdfasdf</p>'
+        '</div>'
+        '<div>'
+        '<p>published: 12.11.2019, 14:39</p>'
+        '<h2><a href="">My Second post</a></h2>'
+        '<p>asdfasdfasdfasdfdfdf</p>'
+        '</div>'
+        '</body>'
+        '</html>'
+    )
